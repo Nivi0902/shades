b59c764447a4371dc6ed64175bd46cf783b2fc3a
nivi0902 <nivetha.s@invicious.in>
2024-12-19 17:41:04 +0530

    19-12-2024 css styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+// const headerStyle = {
+//     color: '#3A3636'
+// }
+
+  return (
+    <header>
+        <h1>My Tasks</h1>
+    </header>
+  )
+}
+
+//style = " grgirhgi"
+//style = {{backgroubsberr:}}
+
+export default Header
